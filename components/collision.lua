408db8b7a32a8ab964a4a56421bfc19478517308
Joseph Newman <paradoxbuilder@gmail.com>
2022-02-28 17:34:41 -0700

    Collision size reduced; can draw self if circle in debug draw mode

--- components/collision.lua
+++ components/collision.lua
@@ -10,7 +10,7 @@ function CollisionComponent:new(entity, bodyType, obj)
 	local valid = entity or error("Cannot set up collider without an entity")
 	collider.body = love.physics.newBody(CurrentWorld.physicsWorld, entity.transform.position.x, entity.transform.position.y, bodyType or "dynamic")
 	collider.body:setLinearDamping(8)
-	collider.shape = love.physics.newCircleShape(10)
+	collider.shape = love.physics.newCircleShape(6)
 	collider.fixture = love.physics.newFixture(collider.body, collider.shape, 1)
 
 	return collider
@@ -18,6 +18,15 @@ end
 
 function CollisionComponent:update(entity, dt)
 	entity.transform.position = Vector(self.body:getX(), self.body:getY())
+end
+
+function CollisionComponent:draw(entity)
+	if DEBUG_MODE and DRAW_DEBUG then
+		local shape = self.shape:getType()
+		if shape == "circle" then
+			love.graphics.circle("line", 0, 0, self.shape:getRadius())
+		end
+	end
 end
 
 function CollisionComponent:applyForce(vector)
