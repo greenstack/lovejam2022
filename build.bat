a35b9dde2ba5bc03b0d4990cac47182ccf221464
Joseph Newman <paradoxbuilder@gmail.com>
2022-03-04 15:26:39 -0700

    build.bat doesn't work lol

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-tar.exe --exclude="bin/*" --exclude="*.bat" --exclude="*.aseprite" -a -c -f bin/game.love . 
+tar.exe --exclude="bin/*" --exclude="*.bat" --exclude="*.aseprite" -a -c -f bin/%1%.love .
+copy /b "C:\Program Files\LOVE\love.exe"+%1%.love "bin/%1%.exe"
